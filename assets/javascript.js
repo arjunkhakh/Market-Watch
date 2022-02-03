4741f7efcb5061a654dff15ee34d160865f8c529
Arjun Singh <arjunkhakh@gmail.com>
2022-02-03 19:01:23 +0000

    putting the fetch information into a submit button event listener function

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,15 +2,15 @@ var inputBox = document.querySelector("#inputBox")
 
 var submitButton = document.querySelector("#submitBtn")
 
-var url = "https://finnhub.io/api/v1/search?q=US0378331005&token=c7sopmqad3i9jn7rii60";
+// var url = "https://finnhub.io/api/v1/search?q=US0378331005&token=c7sopmqad3i9jn7rii60";
 
-var url2 = "https://finnhub.io/api/v1/stock/symbol?exchange=US&token=c7sopmqad3i9jn7rii60"
+// var url2 = "https://finnhub.io/api/v1/stock/symbol?exchange=US&token=c7sopmqad3i9jn7rii60"
 
-var url3 = "https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2021-09-01&to=2021-09-09&token=c7sopmqad3i9jn7rii60"
+// var url3 = "https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2021-09-01&to=2021-09-09&token=c7sopmqad3i9jn7rii60"
 
-// https://finnhub.io/api/v1/search?q=US0378331005&token=c7sopmqad3i9jn7rii60
+// // https://finnhub.io/api/v1/search?q=US0378331005&token=c7sopmqad3i9jn7rii60
 
-var url5 = "https://eodhistoricaldata.com/api/search/Apple%20Inc?api_token=61fc0e9d63bc94.19357754&limit=1"
+// var url5 = "https://eodhistoricaldata.com/api/search/Apple%20Inc?api_token=61fc0e9d63bc94.19357754&limit=1"
 
 function profileCall(url) {
   fetch(url)
@@ -22,8 +22,23 @@ function profileCall(url) {
       });
 }
 
-fetch(url5) 
-.then(function (response) {
+// fetch(url5) 
+// .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     var symbol = data[0].Code;
+//     console.log(data[0].Code);
+//     var profileUrl = ("https://finnhub.io/api/v1/stock/profile2?symbol=" + symbol + "&token=c7sopmqad3i9jn7rii60")
+
+//     profileCall(profileUrl); 
+//   })
+
+  submitButton.addEventListener("click", function () {
+    var input = inputBox.value;
+
+    fetch(("https://eodhistoricaldata.com/api/search/" + input + "?api_token=61fc0e9d63bc94.19357754&limit=1")) 
+  .then(function (response) {
     return response.json();
   })
   .then(function (data) {
@@ -33,6 +48,8 @@ fetch(url5)
 
     profileCall(profileUrl); 
   })
+
+  });
 
 // function profileCall(url) {
 //   fetch(url)
