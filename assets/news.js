82a38f2e56bd7f90310e5db04412225a399f3d0c
Arjun Singh <arjunkhakh@gmail.com>
2022-02-08 21:52:04 +0000

    getting the input box to work for the company news

--- assets/news.js
+++ assets/news.js
@@ -13,6 +13,9 @@ var thumbnail2 = document.querySelector("#thumbnail2")
 var thumbnail3 = document.querySelector("#thumbnail3")
 var thumbnail4 = document.querySelector("#thumbnail4")
 
+var sButton = document.querySelector("#searchBtn")
+var sInput = document.querySelector("#companyBox")
+
 fetch("https://newsdata.io/api/1/news?apikey=pub_4321bed719384d54f676f9caf3e503585594&country=us&category=business&language=en")
 .then(function (response) {
     return response.json();
@@ -42,3 +45,37 @@ fetch("https://newsdata.io/api/1/news?apikey=pub_4321bed719384d54f676f9caf3e5035
     var img4 = news.results[3].image_url
     thumbnail4.setAttribute("src", img4)
   });
+
+sButton.addEventListener("click", function () {
+    var value = sInput.value  
+
+    fetch("https://newsdata.io/api/1/news?apikey=pub_4321bed719384d54f676f9caf3e503585594&q=" + value + "&country=us&language=en&category=business")
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (news) {
+    console.log(news);
+
+    title1.textContent = news.results[0].title
+    title2.textContent = news.results[1].title
+    title3.textContent = news.results[2].title
+    title4.textContent = news.results[3].title
+
+    desc1.textContent = news.results[0].description
+    desc2.textContent = news.results[1].description
+    desc3.textContent = news.results[2].description
+    desc4.textContent = news.results[3].description
+
+    var img1 = news.results[0].image_url
+    thumbnail1.setAttribute("src", img1)
+
+    var img2 = news.results[1].image_url
+    thumbnail2.setAttribute("src", img2)
+
+    var img3 = news.results[2].image_url
+    thumbnail3.setAttribute("src", img3)
+
+    var img4 = news.results[3].image_url
+    thumbnail4.setAttribute("src", img4)
+  });
+  })
