1bf7d420b08f1aa6f1dfa02a271db15aa8168706
Arjun Singh <arjunkhakh@gmail.com>
2022-02-08 20:42:50 +0000

    sorting out the ids in the HTML + getting the fetch command to get the ticker for the companies to put into the urls

--- assets/javascript.js
+++ assets/javascript.js
@@ -30,12 +30,6 @@ var ticker11 = document.querySelector("#ticker11")
 var ticker12 = document.querySelector("#ticker12")  
 var ticker13 = document.querySelector("#ticker13") 
 
-var ticker1 = document.querySelector("#ticker1") // Apple
-var ticker2 = document.querySelector("#ticker2") // Tesla 
-var ticker3 = document.querySelector("#ticker3") // Amazon
-var ticker4 = document.querySelector("#ticker4") // Google
-var ticker5 = document.querySelector("#ticker5") // Netflix
-
 function profileCall(url) {
   fetch(url)
       .then(function (response) {
@@ -44,9 +38,9 @@ function profileCall(url) {
       .then(function (data) {
         console.log(data);
 
-        var name = data[0].name;
-        var ticker = data[0].ticker;
-        var logo = data[0].logo;
+        var name = data.name;
+        var ticker = data.ticker;
+        var logo = data.logo;
 
         console.log(name);
         console.log(ticker)
@@ -54,136 +48,21 @@ function profileCall(url) {
       });
 }
 
-// Apple Ticker and Stock Price
-function cardInfo1() {
-  fetch("https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&token=c7sopmqad3i9jn7rii60")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    ticker1.textContent = data.ticker;
-  })
-
-  fetch("https://finnhub.io/api/v1/quote?symbol=AAPL&token=c7sopmqad3i9jn7rii60")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    price1.textContent = "$" + data.c;
-  })
-}
-
-// Tesla ticker and Stock Price
-function cardInfo2() {
-  fetch("https://finnhub.io/api/v1/stock/profile2?isin=US88160R1014&token=c7sopmqad3i9jn7rii60")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    ticker2.textContent = data.ticker;
-  })
-
-  fetch("https://finnhub.io/api/v1/quote?symbol=TSLA&token=c7sopmqad3i9jn7rii60")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    price2.textContent = "$" + data.c;
-  })
-}
-
-// Amazon ticker and Stock Price
-function cardInfo3() {
-  fetch("https://finnhub.io/api/v1/stock/profile2?isin=US0231351067&token=c7sopmqad3i9jn7rii60")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    ticker3.textContent = data.ticker;
-  })
-
-  fetch("https://finnhub.io/api/v1/quote?symbol=AMZN&token=c7sopmqad3i9jn7rii60")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    price3.textContent = "$" + data.c;
-  })
-}
-
-// Google Ticker and Stock Price
-function cardInfo4() {
-  fetch("https://finnhub.io/api/v1/stock/profile2?symbol=GOOG&token=c7sopmqad3i9jn7rii60")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    ticker4.textContent = data.ticker;
-  })
-
-  fetch("https://finnhub.io/api/v1/quote?symbol=GOOGL&token=c7sopmqad3i9jn7rii60")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    price4.textContent = "$" + data.c;
-  })
-}
-
-// Netflix Ticker and Stock Price
-function cardInfo5() {
-  fetch("https://finnhub.io/api/v1/stock/profile2?isin=US64110L1061&token=c7sopmqad3i9jn7rii60")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    ticker5.textContent = data.ticker;
-  })
-
-  fetch("https://finnhub.io/api/v1/quote?symbol=NFLX&token=c7sopmqad3i9jn7rii60")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    price5.textContent = "$" + data.c;
-  })
-}
-
 function stockPrice(url) {
   fetch(url)
       .then(function (response) {
         return response.json();
       })
-      .then(function (data) {
-        console.log(data);
+      .then(function (stockValue) {
+        console.log(stockValue);
 
-        var Current = data.c; // Current Price
-        var change = data.d; // Change 
-        var percentage = data.dp; // Percentage in Change
-        var high = data.h; // High Price of the day
-        var low = data.l; // Low Pric of the day
-        var open = data.o; // Open price of the day
-        var previous = data.pc; // Previous close price
+        var Current = stockValue.c; // Current Price
+        var change = stockValue.d; // Change 
+        var percentage = stockValue.dp; // Percentage in Change
+        var high = stockValue.h; // High Price of the day
+        var low = stockValue.l; // Low Pric of the day
+        var open = stockValue.o; // Open price of the day
+        var previous = stockValue.pc; // Previous close price
 
         console.log(Current)
         console.log(change)
@@ -521,13 +400,21 @@ cardInfo13()
 submitButton.addEventListener("click", function () {
   var input = inputBox.value;
 
-  fetch(("https://eodhistoricaldata.com/api/search/" + input + "?api_token=61fc0e9d63bc94.19357754&limit=1")) 
+  fetch(("https://api.polygon.io/v3/reference/tickers?type=CS&search=" + input + "&active=true&sort=ticker&order=asc&limit=1&apiKey=MkOWSOfBprqN9JwzoYOPF7kn6gjM3gML")) 
 .then(function (response) {
   return response.json();
 })
-.then(function (data) {
-  var symbol = data[0].Code;
-  console.log(data[0].Code);
+.then(function (input) {
+  var symbol = input.results[0].ticker
+  console.log(input.results[0].ticker);
+  
+  var profileUrl = ("https://finnhub.io/api/v1/stock/profile2?symbol=" + symbol + "&token=c7sopmqad3i9jn7rii60")
+
+  profileCall(profileUrl); 
+
+   var stockUrl = ("https://finnhub.io/api/v1/quote?symbol=" + symbol + "&token=c7sopmqad3i9jn7rii60");
+
+   stockPrice(stockUrl);
 });
 });
 
